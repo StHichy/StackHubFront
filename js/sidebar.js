a1b75e0a65807b90c975d5883923a72b01269891
Carlos Henrique <stormhichy@gmail.com>
2025-08-28 09:03:35 -0300

    Layout sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,18 @@
+$(document).ready(function () {
+  // Carregar sidebar externa
+  $("#sidebar-container").load("sidebar.html", function () {
+    const $sidebar = $(".sidebar");
+    const $mainContent = $(".main-content");
+
+    $sidebar.addClass("active"); 
+
+    $("#conversa").on("click", function () {
+      $sidebar.toggleClass("active");
+      $mainContent.toggleClass("shrink");
+    });
+  });
+});
+
 const conversaBtn = document.getElementById("conversa");
 const sidebar = document.querySelector(".sidebar");
 const mainContent = document.querySelector(".main-content");
